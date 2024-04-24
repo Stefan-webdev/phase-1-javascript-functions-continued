bd85be7793160a034a6637cceebbc3ea4c293260
Stefan-webdev <154948992+Stefan-webdev@users.noreply.github.com>
2024-04-24 15:52:00 +0930

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+return `This Saturday, I want to ${activity}!`;
+
+} 
+
+console.log(saturdayFun());
+//console.log(saturdayFun("bike"));
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+console.log(mondayWork());
+
+function wrapAdjective(flair = "*") {
+    return function(adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Test cases
+  const highlightStar = wrapAdjective("*");
+  const highlightBars = wrapAdjective("||");
+  
+  console.log(highlightStar("awesome")); // Output: "You are *awesome*!"
+  console.log(highlightBars("fantastic")); // Output: "You are ||fantastic||!"
+
